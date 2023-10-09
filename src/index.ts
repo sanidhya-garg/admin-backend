b8efa652b6689c4f49c28b826485495c2b844f4d
Arush Bansal <arush4bansal@gmail.com>
2023-10-09 20:34:07 +0530

    authentiction middleware

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
+import dotenv from "dotenv"
+dotenv.config()
+
 import express  from "express";
+import { authenticationMiddleware } from "./authentication";
 
 const app = express()
 app.use(express.json({ limit: '1mb' }))
 app.use(express.urlencoded({extended : true})) // TODO 
+app.use(authenticationMiddleware)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Listening on Port ${PORT}`))
