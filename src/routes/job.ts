b05684469465216a5b70c9950933ae940c9661a9
Arush Bansal <arush4bansal@gmail.com>
2023-10-18 16:41:38 +0530

    admin approval integration complete

--- src/routes/job.ts
+++ src/routes/job.ts
@@ -3,6 +3,20 @@ import { Job } from '../database/models';
 
 const job_router = express.Router();
 
+
+job_router.post("/", async (req : Request, res : Response) => {
+    try{
+        const JobList = await Job.find();
+        res.json(JobList)
+    }
+    catch (error : any) {
+        console.log("error in job.ts GET /")
+        console.log(error)
+        res.status(500).send({message : "Server error", error : error.message || "Internal Server Error"})
+
+    }
+})
+
 job_router.put('/:job_id/approval', async (req: Request, res: Response) => {
     try {
         const jobId: string = req.params.job_id;
