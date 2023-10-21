a0725e095115134748f9a4ecd31914ab82dc5779
Aditya Bisen <adityabisenn@gmail.com>
2023-10-21 17:58:41 +0530

    jwt in admin portal

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,9 @@ const app = express()
 app.use(cors())
 app.use(express.json({ limit: '1mb' }))
 app.use(express.urlencoded({extended : true})) // TODO 
+app.use('/auth', auth_router)
 app.use(authenticationMiddleware)
 app.use('/job', job_router)
-app.use('/auth', auth_router)
 
 mongoConnecter.connect(process.env.DATABASE_URL || "")
 
